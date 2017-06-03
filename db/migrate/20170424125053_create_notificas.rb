32542a76161b3a01c16979a70d44d0f2520164fe
flaviomax94 <flaviomax94.fmf@gmail.com>
2017-06-03 19:37:58 +0200

    fix migrate

--- db/migrate/20170424125053_create_notificas.rb
+++ db/migrate/20170424125053_create_notificas.rb
@@ -2,7 +2,7 @@ class CreateNotificas < ActiveRecord::Migration[5.0]
   def change
     create_table :notificas do |t|
       t.references :user, foreign_key: true
-      t.references :notified_by, foreign_key: true
+      t.integer :notified_by, foreign_key: true
       t.string :tipo , default: false
       t.boolean :read
 
