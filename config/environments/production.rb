7aba7dba3ce02220b2dc9c2973ecd90fab97fbd0
flaviomax94 <flaviomax94.fmf@gmail.com>
2017-06-05 13:39:31 +0200

    Url Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,15 @@ config.serve_static_assets = true
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "CarAdvisor_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  ActionMailer::Base.smtp_settings = {
+  :user_name => [SENDGRID_USERNAME],
+  :password => [SENDGRID_PASSWORD],
+  :domain => 'https://car-advisors.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
